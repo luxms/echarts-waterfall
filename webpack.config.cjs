c1d9f141acda89c7aed58801381c630becb048b1
Sofya Shakirova <sshakirova@pc029.spb.luxms.com>
2024-07-30 16:27:09 +0300

    webpacky

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -14,7 +14,12 @@ module.exports = (env, options) => {
     },
     devtool: 'source-map',
     externals: {
-      'echarts/lib/echarts': 'echarts'
+      'echarts/lib/echarts': 'echarts',
+      'zrender/lib/core/util': 'zrender',
+      'zrender/lib/core': 'zrender',
+      'zrender': 'zrender',
+      'zrender/lib/core/platform.js': 'zrender/lib/core/platform.js',
+      './node_modules/zrender/lib/core/matrix.js': './node_modules/zrender/lib/core/matrix.js'
     }
   };
 };
